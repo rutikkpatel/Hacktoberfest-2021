01e273dcbd5f197be029d3578af461c1cfb6ad49
Rutik K Patel <72887393+rutikkpatel@users.noreply.github.com>
2021-10-15 00:16:37 +0530

    Create Height_Calculator.c

---
+++ C Language/Height_Calculator.c
@@ -0,0 +1,67 @@
+// C Program to Convert height
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+void hicm()
+{
+	float no,nocov;
+	printf("Enter your height :");
+	scanf("%f", &no);
+	nocov=no/30.48;
+	printf("\nYour height in feet is %f",nocov);
+}
+void hift()
+{
+	float no,nocov;
+	printf("Enter your height :");
+	scanf("%f", &no);
+	nocov=no*30.48;
+	printf("\nYour height in cm is %f",nocov);
+}
+void yes()
+{
+	printf("1) From cm to feet \n2) For feet to cm\n3) Exit");
+}
+
+void no()
+{
+	printf("Thank You!!!");
+}
+int main()
+{
+	int ch,ch1;
+	
+	printf("Do you want to know your height ?\n");
+	printf("1) From cm to feet \n2) For feet to cm \n3) Exit");
+	while(1)
+	{
+		printf("\nEnter Your Choice: ");
+		scanf("%d",&ch);
+		
+		switch(ch)
+		{
+			case 1:hicm();
+			       break;       
+			case 2:hift();
+			       break;		
+			case 3:exit(0);	
+			default:printf("Invalid Choice\n");	        
+ 	    }
+ 	    
+ 	    printf("\nDo you want to print menu again ( 0=yes / 1=No)");
+ 	    
+ 	    printf("\nEnter Your Choice :  \n");
+ 	    scanf("%d",&ch1);
+ 	    
+ 	    switch(ch1)
+ 	    {
+ 	    	case 0:yes();
+ 	    	       break;
+ 	    	case 1:no();
+ 	    	       exit(0);
+			       break;     
+			default:printf("Incorrect Value");	        
+		 }
+	}
+	return 0;
+}
